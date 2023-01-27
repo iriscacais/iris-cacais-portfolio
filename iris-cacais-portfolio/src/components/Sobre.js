59ff16e1373726727e956631647562948b11377d
iriscacais <mandeparairis@gmail.com>
2023-01-26 21:10:33 -0300

    atualiza o sobre

--- iris-cacais-portfolio/src/components/Sobre.js
+++ iris-cacais-portfolio/src/components/Sobre.js
@@ -9,7 +9,8 @@ class Sobre extends React.Component {
             <section >
               <div className="divImage" >
                 <img id="sobreMim"src='./iris.png' alt="Foto de Iris" className="foto" /> 
-                <p>Morbi cursus tortor neque, porttitor facilisis lorem gravida et. Mauris in est a dui convallis sagittis sit amet ut quam. Duis vehicula dolor et ligula finibus, ac eleifend augue imperdiet. Vestibulum ac rhoncus mauris. Duis ac egestas leo. Etiam non nibh in ante iaculis dapibus eget eu orci. Vivamus luctus pulvinar libero, in vestibulum eros rutrum in. Vestibulum tincidunt sed ipsum sit amet fringilla. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+                <p>Meu nome é Iris e tenho 29 anos, em setembro de 2022 iniciei uma transição de carreira na escola de desenvolvimento web Trybe. Amo estudar e aprender coisas novas e por isso minha maior motivação em entrar na área de tecnologia é poder exercer minha criatividade e curiosidade em ambientes colaborativos com grande diversidade de pessoas. Conclui o módulo de fundamentos em novembro e atualmente estou estudando desenvolvimento front-end, no curso tenho oportunidade de praticar as habilidades adquiridas em diversos exercícios e projetos individuais e em grupo.
+                </p>
               </div>
             </section>
             
--- iris-cacais-portfolio/src/dataProjetos.js
+++ iris-cacais-portfolio/src/dataProjetos.js
@@ -3,7 +3,7 @@ const dataProjetos = [
         id: 1,
         name:'Solar System',
         description: 'Modelo de Sistema Solar utilizando React. - Front-end',
-        details: 'react, jsx, css3',
+        details: 'React, JSX, CSS3',
         image:'/images/solarSystem1.png',
         github:'https://github.com/iriscacais/solar-system',
         deploy:'https://known-wood.surge.sh/',
@@ -16,7 +16,25 @@ const dataProjetos = [
         details: 'ES6, HOFs, testes unitários',
         image:'/images/zoo.jpeg',
         github:'https://github.com/iriscacais/zoo-functions',
+    },
+
+    {
+        id: 3,
+        name:'Shopping cart',
+        description: 'Desenvolvimento de um carrinho de compras dinâmico - Front-end',
+        details: 'JavaScript, API, promises',
+        image:'/images/shoppingcart.png',
+        github:'https://shoppingcartiris.netlify.app/',
         deploy:'',
+    },
+
+    {
+        id: 4,
+        name:'Unit tests',
+        description: 'Implementação de funções e testes unitários. - Fundamentos',
+        details: 'ES6, Jest, testes unitários',
+        image:'/images/unit.png',
+        github:'https://github.com/iriscacais/unit-tests',
     },
 
 ]
